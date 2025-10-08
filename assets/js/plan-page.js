d8d6a72edee86193c38fd6a0791c816f585f4f65
Christian-Porol <chrislorop@gmail.com>
2025-10-08 16:53:25 +0800

    Deleted plan page shortcode along with associated files

--- assets/js/plan-page.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-    $(".plan-button").on("click", function() {
-        let plan = $(this).closest(".plan-box").find("h2").text();
-        alert("You selected: " + plan + " (payment integration coming soon)");
-    });
-});
